73b5ec5c06711b689d21a719c0c062a11755b9b1
ivannizar <71221969+dragranzer@users.noreply.github.com>
2022-02-16 20:26:19 +0700

    Update Profile.js

--- src/user_pages/Profile.js
+++ src/user_pages/Profile.js
@@ -20,62 +20,62 @@ function Profile() {
     
     // console.log(token)
     const URL = `http://47.254.198.205/api/users?append=large_image`
-    useEffect(() =>  {
-        if(token){
-            const getData = async () => {
-                Axios.get(URL, { headers: {"Authorization" : `Bearer ${token}`} })
-                .then(res => {
-                console.log(res);
-                setProfile(res.data.data);
-                if(res.data["data"].token){
-                    console.log("berhasil")
-                }
-                }).catch(error => {
-                    // this.setError()
-                    console.log(error)
-                    if (error.response) {
-                        console.log("--------------------------------------------------")
-                        // The request was made and the server responded with a status code
-                        // that falls out of the range of 2xx
-                        console.log(error.response.data);
-                        console.log(error.response.status);
-                        if(error.response.status === 401){
-                            history.push("/");
-                            swal({
-                                title: "Error",
-                                text: "Mohon Login Terlebih Dahulu",
-                                icon: "error",
-                            });
-                        }
-                        console.log(error.response.headers);
-                    } else if (error.request) {
-                        console.log("*************************")
+//     useEffect(() =>  {
+//         if(token){
+//             const getData = async () => {
+//                 Axios.get(URL, { headers: {"Authorization" : `Bearer ${token}`} })
+//                 .then(res => {
+//                 console.log(res);
+//                 setProfile(res.data.data);
+//                 if(res.data["data"].token){
+//                     console.log("berhasil")
+//                 }
+//                 }).catch(error => {
+//                     // this.setError()
+//                     console.log(error)
+//                     if (error.response) {
+//                         console.log("--------------------------------------------------")
+//                         // The request was made and the server responded with a status code
+//                         // that falls out of the range of 2xx
+//                         console.log(error.response.data);
+//                         console.log(error.response.status);
+//                         if(error.response.status === 401){
+//                             history.push("/");
+//                             swal({
+//                                 title: "Error",
+//                                 text: "Mohon Login Terlebih Dahulu",
+//                                 icon: "error",
+//                             });
+//                         }
+//                         console.log(error.response.headers);
+//                     } else if (error.request) {
+//                         console.log("*************************")
             
-                        // The request was made but no response was received
-                        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                        // http.ClientRequest in node.js
-                        console.log(error.request);
-                    } else {
-                        console.log("++++++++++++++++++++++++")
-                        // Something happened in setting up the request that triggered an Error
-                        console.log('Error', error.message);
-                    }
-                    console.log(error.config);
-                })
-            }
-            getData();
-            console.log(profile)
-        }
-    },[]);
+//                         // The request was made but no response was received
+//                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+//                         // http.ClientRequest in node.js
+//                         console.log(error.request);
+//                     } else {
+//                         console.log("++++++++++++++++++++++++")
+//                         // Something happened in setting up the request that triggered an Error
+//                         console.log('Error', error.message);
+//                     }
+//                     console.log(error.config);
+//                 })
+//             }
+//             getData();
+//             console.log(profile)
+//         }
+//     },[]);
     console.log(profile)
-    if(!token){
-        history.push("/");
-        swal({
-            title: "Error",
-            text: "Mohon Login Terlebih Dahulu",
-            icon: "error",
-        });
-    }
+//     if(!token){
+//         history.push("/");
+//         swal({
+//             title: "Error",
+//             text: "Mohon Login Terlebih Dahulu",
+//             icon: "error",
+//         });
+//     }
 
     const onChange = (e) =>{
         const name = e.target.name;
